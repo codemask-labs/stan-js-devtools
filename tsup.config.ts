1feff965d09b63cca6076af88cd3f2ab4cf5c48e
Hubert Bieszczad <brentlokk@gmail.com>
2024-08-01 13:54:00 +0200

    chore: build rn separated

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-    entry: ['src/index.ts'],
+    entry: ['src/index.ts', 'src/rn/index.ts'],
     dts: true,
     format: ['esm', 'cjs'],
     clean: true,
