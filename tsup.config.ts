f523a96e25f170aa595b331f9a6fc2cc58b1765f
Hubert Bieszczad <brentlokk@gmail.com>
2024-08-13 14:18:24 +0200

    feat: inject css into bundles

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,5 @@
 // tsup.config.ts
+import { readFile, writeFile } from 'fs/promises'
 
 import { defineConfig } from 'tsup'
 
@@ -8,4 +9,19 @@ export default defineConfig({
     format: ['esm', 'cjs'],
     clean: true,
     sourcemap: false,
+    onSuccess: async () => {
+        const mjs = await readFile('dist/index.js', 'utf8')
+        const mjsLines = mjs.split('\n')
+        const mjsInjected = [mjsLines.at(0)]?.concat('import \'./index.css\';', mjsLines.slice(1)).join('\n')
+
+        await writeFile('dist/index.js', mjsInjected)
+
+        const cjs = await readFile('dist/index.cjs', 'utf8')
+        const cjsLines = cjs.split('\n')
+        const cjsInjected = [cjsLines.at(0)]?.concat('require(\'./index.css\');', cjsLines.slice(1)).join('\n')
+
+        await writeFile('dist/index.cjs', cjsInjected)
+
+        console.log('⚡️ Injected CSS into bundles ⚡️')
+    },
 })
